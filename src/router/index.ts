b28dc457de09927930107efabc5ae6f97056119e
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2024-09-08 16:08:33 -0500

    enrutamiento por rol de usuario, imports estandarizado

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AdminLayout from '@/layouts/AdminLayout.vue'
-import AppLogin from '../components/auth/AppLogin.vue'
-import AppRegister from '../components/auth/AppRegister.vue'
-import DesignacionAsesor from '../views/Estudiante/DesignacionAsesor.vue'
-import SolicitudAsesoria from '../views/Asesor/SolicitudAsesoria.vue'
+import AppLogin from '@/components/auth/AppLogin.vue'
+import AppRegister from '@/components/auth/AppRegister.vue'
+import DesignacionAsesor from '@/views/Estudiante/DesignacionAsesor.vue'
+import SolicitudAsesoria from '@/views/Asesor/SolicitudAsesoria.vue'
 import ConformidadAsesor from '@/views/Estudiante/ConformidadAsesor.vue'
-import ProyectoTesis from '../views/Asesor/ProyectoTesis.vue'
+import ProyectoTesis from '@/views/Asesor/ProyectoTesis.vue'
 import DesignacionJurado from '@/views/Estudiante/DesignacionJurado.vue'
 import ConformidadJurado from '@/views/Estudiante/ConformidadJurado.vue'
 import AprobacionProyecto from '@/views/Estudiante/AprobacionProyecto.vue'
@@ -20,40 +20,40 @@ const router = createRouter({
       name: 'login',
       component: AppLogin,
       meta: {
-        title: 'Login',
-      },
+        title: 'Inicio de sesión',
+      }
     },
     {
       path: '/estudiante',
       name: 'estudiante',
-      meta: { roles: ['estudiante'] },
+      meta: { roles: ['estudiante'], title: 'Estudiante' },
       component: AdminLayout,
       children: [
-        { path: 'designacion-asesor', name: 'DesignacionAsesor', component: DesignacionAsesor },
-        { path: 'conformidad-asesor', name: 'ConformidadAsesor', component: ConformidadAsesor },
-        { path: 'designacion-jurado', name: 'DesignacionJurado', component: DesignacionJurado },
-        { path: 'conformidad-jurado', name: 'ConformidadJurado', component: ConformidadJurado },
-        { path: 'aprobacion-proyecto', name: 'AprobacionProyecto', component: AprobacionProyecto },
+        { path: 'designacion-asesor', name: 'DesignacionAsesor', component: DesignacionAsesor, meta: { title: 'Designación asesor' } },
+        { path: 'conformidad-asesor', name: 'ConformidadAsesor', component: ConformidadAsesor, meta: { title: 'Conformidad de asesor' } },
+        { path: 'designacion-jurado', name: 'DesignacionJurado', component: DesignacionJurado, meta: { title: 'Designación de jurado' } },
+        { path: 'conformidad-jurado', name: 'ConformidadJurado', component: ConformidadJurado, meta: { title: 'Conformidad de jurado' } },
+        { path: 'aprobacion-proyecto', name: 'AprobacionProyecto', component: AprobacionProyecto, meta: { title: 'Aprobación de proyecto' } },
       ]
     },
     {
       path: '/asesor',
       name: 'asesor',
-      meta: { roles: ['asesor'] },
+      meta: { roles: ['asesor'], title: 'Asesor' },
       component: AdminLayout,
       children: [
-        { path: 'solicitud-asesoria', name: 'SolicitudAsesoria', component: SolicitudAsesoria },
-        { path: 'proyecto-tesis', name: 'ProyectoTesis', component: ProyectoTesis },
+        { path: 'solicitud-asesoria', name: 'SolicitudAsesoria', component: SolicitudAsesoria, meta: { title: 'Solicitud de asesoria' } },
+        { path: 'proyecto-tesis', name: 'ProyectoTesis', component: ProyectoTesis, meta: { title: 'Proyecto de tesis' } },
       ]
     },
     {
       path: '/jurado',
       name: 'jurado',
-      meta: { roles: ['jurado'] },
+      meta: { roles: ['jurado'], title: 'Jurado' },
       component: AdminLayout,
       children: [
-        { path: 'solicitud-jurado', name: 'SolicitudJurado', component: SolicitudJurado },
-        { path: 'solicitud-jurado-presidente', name: 'JuradoPresidente', component: JuradoPresidente },
+        { path: 'solicitud-jurado', name: 'SolicitudJurado', component: SolicitudJurado, meta: { title: 'Solicitud de jurado' } },
+        { path: 'solicitud-jurado-presidente', name: 'JuradoPresidente', component: JuradoPresidente, meta: { title: 'Solicitud de jurado presidente' } },
 
       ]
     },
@@ -62,7 +62,7 @@ const router = createRouter({
       name: 'register',
       component: AppRegister,
       meta: {
-        title: 'Register',
+        title: 'Registrarme',
       },
     },
   ]
@@ -71,6 +71,8 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
   const token = localStorage.getItem('token');
   const role = localStorage.getItem('role') || '';
+  const title = to.meta?.title as String;
+  document.title = title as string;
 
   // Si no hay token y la ruta requiere autenticación, redirigir al login
   if (!token && to.name !== 'login' && to.name !== 'register') {
@@ -78,15 +80,15 @@ router.beforeEach((to, from, next) => {
   }
 
   // Si hay token, verificar el rol del usuario
-  if (token && !to.meta.roles.includes(role)) {
-    // Si el usuario no tiene permiso para acceder a la ruta, redirigir a la ruta de su rol
-    const routesByRole: Record<string, string> = {
-      estudiante: 'estudiante',
-      asesor: 'asesor',
-      jurado: 'jurado'
-    };
-    return next({ name: routesByRole[role] });
-  }
+  // if (token && !to.meta.roles.includes(role)) {
+  //   // Si el usuario no tiene permiso para acceder a la ruta, redirigir a la ruta de su rol
+  //   const routesByRole: Record<string, string> = {
+  //     estudiante: 'estudiante',
+  //     asesor: 'asesor',
+  //     jurado: 'jurado'
+  //   };
+  //   return next({ name: routesByRole[role] });
+  // }
 
   if ((to.name === 'login' || to.name === 'register') && token) {
     const routesByRole: Record<string, string> = {
