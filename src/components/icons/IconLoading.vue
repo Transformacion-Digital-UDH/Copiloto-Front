29c83a12fe9bdfd27473c5cc0773686cda0f3ac8
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2024-09-12 18:28:55 -0500

    soporte de autenticacion con google

---
+++ src/components/icons/IconLoading.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    class="animate-spin h-5 w-5 mr-2 text-white"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+  >
+    <circle
+      class="opacity-25"
+      cx="12"
+      cy="12"
+      r="10"
+      stroke="currentColor"
+      stroke-width="4"
+    />
+    <path
+      class="opacity-75"
+      fill="currentColor"
+      d="M4 12a8 8 0 0116 0 8 8 0 01-16 0z"
+    />
+  </svg>
+</template>
