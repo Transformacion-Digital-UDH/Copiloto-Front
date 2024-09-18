87db6a0fb57d19c8d7567b960bb31fc3f3984d3a
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2024-09-18 12:55:04 -0500

    setup pinia with plugin persisted state

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
 import './assets/css/main.css'
-
 import '@fortawesome/fontawesome-free/css/all.css';
-
-import { createApp } from 'vue'
+import { createApp, markRaw } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from '@/App.vue'
 import router from '@/router'
 import axios from 'axios';
 import vue3GoogleLogin from 'vue3-google-login'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 axios.defaults.baseURL = 'https://titulacion-back.abimaelfv.site';
+
+const pinia = createPinia()
+
+pinia.use(({store}) => {
+    store.router = markRaw(router)
+})
+
+pinia.use(piniaPluginPersistedstate)
 
 const app = createApp(App)
 
@@ -18,7 +24,7 @@ app.use(vue3GoogleLogin, {
     clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID
 })
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
