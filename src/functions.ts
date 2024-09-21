2ae8de98414ecfe0949b773e1689d6b732ae4042
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2024-09-21 12:43:18 -0500

    agregando animacion confetti al realizar peticion

--- src/functions.ts
+++ src/functions.ts
@@ -1,6 +1,7 @@
 import Swal, { type SweetAlertIcon } from 'sweetalert2';
 import { useAuthStore } from './stores/auth';
 import axios from 'axios';
+import confetti from 'canvas-confetti';
 
 export const alertToast = (message: string, title: string, icon: string = 'question') => {
     const toast = Swal.mixin({
@@ -21,20 +22,23 @@ export const alertToast = (message: string, title: string, icon: string = 'quest
     })
 }
 
-export const alertConfirmation = (message: string, title: string, icon: string = 'question') => {
+export const alertConfirmation = (message: string, title: string, icon: string = 'question', params: any, url: string, redirect: string, method: string) => {
     const alert = Swal.mixin({buttonsStyling: true})
     alert.fire({
         title: title,
         icon: icon as SweetAlertIcon,
         text: message,
         customClass: {
-            confirmButton: 'bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-700'
+            confirmButton: 'bg-base text-white px-4 py-2 rounded-md hover:bg-emerald-600',
+            cancelButton: 'bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700'
         },
+        showCloseButton: true,
+        showCancelButton: true,
         confirmButtonText: 'Sí, acepto',
         cancelButtonText: 'No',
     }).then((result) => {
         if(result.isConfirmed){
-            return 
+            sendRequest(method, url, params, redirect);
         }
     })
 }
@@ -43,23 +47,29 @@ export const sendRequest = async (method: string, url: string, params: any, redi
     const authStore = useAuthStore();
     axios.defaults.headers.common['Authorization'] = `Bearer ${authStore.token}`;
     let res = '';
-
+    console.log(method, url, params);
+    
     await axios({
         method: method,
         url: url,
         data: params
     }).then((response) => {
         res = response.data.status;
-        alertToast(response.data.message, 'Todo OK', 'success');
+        confetti({
+              particleCount: 300,
+              spread: 1010,
+              origin: { y: 0.6 }
+            });
+        alertToast(response.data.message, 'Éxito', 'success');
         setTimeout(() => {
             redirect !== '' ? window.location.href = redirect : '';
-        },1000);
+        }, 3000);
     }).catch((error) => {
         let description = '';
         error.response.data.error.map((e: any) => {
             description = description + ' ' + e
         })
-        alertToast(description, 'Todo BAD', 'error');
+        alertToast(description, 'error', 'error');
     });
     return res;
 }
