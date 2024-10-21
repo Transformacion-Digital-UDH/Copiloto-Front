d65b77aa39f69c5377e4f6a62ccac4fa775b3b01
LUCIANO131231 <2018110461@udh.edu.pe>
2024-10-21 12:17:50 -0500

    arreglando el error

--- src/views/Estudiante/AprobacionProyecto.vue
+++ src/views/Estudiante/AprobacionProyecto.vue
@@ -8,7 +8,7 @@ import { alertToast } from "@/functions";
 const load = ref(false);
 
 // ***** Texto que se escribe automáticamente ********
-const text = "Aprobación del Proyecto de Tesis";
+const text = "Aprobación del proyecto de tesis";
 const textoTipiado2 = ref("");
 let index = 0;
 const typeWriter = () => {
@@ -54,61 +54,50 @@ const estadoClase = (estado: string) => {
 
 ///////////////////////////// CONEXION CON EL BACKEND //////////////////////////
 const authStore = useAuthStore();
-const solicitudEstado = ref<string>("No Iniciado"); 
-const isLoading = ref<boolean>(false);  
-axios.defaults.headers.common["Authorization"] = `Bearer ${authStore.token}`;
+const solicitudEstado = ref<string>(""); 
+const isLoading = ref(false); 
 
-// Función para cargar el estado actual de la solicitud desde el backend
-const cargarEstadoSolicitud = async () => {
-  try {
-    const response = await axios.get(`/api/oficio/estado-solicitud-tesis/${authStore.id}`);
-    console.log("Estado actual de la solicitud:", response.data);
-
-    // Asumimos que el backend devuelve el estado como "Pendiente", "Hecho", o "No Iniciado"
-    solicitudEstado.value = response.data.estado || "No Iniciado";  // Establecemos el estado inicial desde el backend
-
-    if (solicitudEstado.value === "Hecho") {
-      alertToast("La solicitud ya fue realizada y está aprobada.", "Información", "info");
-    }
-  } catch (error) {
-    console.error("Error al cargar el estado de la solicitud:", error);
-    alertToast("No se pudo obtener el estado actual de la solicitud", "Error", "error");
-  }
-};
-
-// Función para realizar la solicitud de aprobación
-const primeraRevision = async () => {
-  if (isLoading.value || solicitudEstado.value === "Hecho" || solicitudEstado.value === "pendiente") return;
-
-  isLoading.value = true;
-  solicitudEstado.value = "pendiente";  // Cambiamos el estado mientras enviamos la solicitud
-
-  try {
-    const response = await axios.post(`/api/oficio/solicitud-aprobar-tesis/${authStore.id}`);  // Cambia la URL si es necesario
-
-    // Verificamos el resultado de la solicitud
-    if (response.data.status) {
-      solicitudEstado.value = "Hecho";  // Si todo va bien, el estado se cambia a "Hecho"
-      alertToast("Aprobación de Tesis creada correctamente", "Éxito", "success");
-    } else {
-      solicitudEstado.value = "pendiente";  // Si falla algo, sigue en pendiente
-    }
-  } catch (error) {
-    console.error("Error en la solicitud de aprobación:", error);
-    alertToast(error.response?.data?.message || "Error al enviar la solicitud", "Error", "error");
-    solicitudEstado.value = "No Iniciado";  // Volvemos al estado inicial si falla
-  } finally {
-    isLoading.value = false;  // Finalizamos la carga
-  }
-};
-
-
-// Cargar el estado de la solicitud al montar el componente
-onMounted(() => {
-  cargarEstadoSolicitud();  // Cargar el estado desde el backend cuando se monta el componente
+// para que el botón quede deshabilitado
+const isAprobacionDisabled = computed(() => {
+  const estadoSolicitud = solicitudEstado.value?.toLowerCase();
+  return ["", ""].includes(estadoSolicitud);// se deshabilita el botón dependiendo del estado
 });
 
+// Función para cargar el estado actual de la solicitud desde el backend
+const solicitarAprobacion = async () => {
+  isLoading.value = true;
+  try {
+    const response = await axios.get(`/api/oficio/solicitud-aprobar-tesis/${authStore.id}`);
+    console.log("Mostrando lo recibido: ",response);
+
+    if (response.data.estado){
+      solicitudEstado.value = "pendiente";
+      alertToast("Solicitud enviada correctamente, espere las indicaciones", "Éxito", "success");
+    }
+
+  } catch (error: any) {
+    console.error(error);
+
+    // Asegúrate de manejar errores correctamente basándote en la respuesta del backend
+    if (error.response?.status == 404) {
+      const message = error.response?.data?.mensaje;
+
+      if (message.includes("conformidad")) {
+        alertToast("Estimado estudiante, no tiene la conformidad de sus jurados", "warning");
+      } else if (message.includes("solicitud de aprobación de tesis pendiente")) {
+        alertToast("Estimado estudiante, ya solicitó la aprobación de su proyecto", "success");
+      } else {
+        alertToast(message || "Error desconocido en la solicitud.", "Error", "error");
+      }
+    } else {
+      alertToast("Error al conectar con el servidor", "Error", "error");
+    }
+  } finally {
+    isLoading.value = false;
+  }
+};
 </script>
+
 <template>
   <template v-if="load">
     <div class="flex-1 p-10 border-s-2 bg-gray-100">
@@ -184,77 +173,56 @@ onMounted(() => {
   
   <template v-else>
     <div class="flex-1 p-10 border-s-2 font-Roboto bg-gray-100">
-      <h3 class="text-5xl font-bold text-center text-azul">
-        {{ textoTipiado2 }}
-      </h3>
-      <br>
-       <!-- Card 1: Solicitud-->
-    <div class="bg-white rounded-lg shadow-lg p-6 relative">
-      <div class="relative flex items-center">
-        <h2 class="text-2xl font-medium text-black">
-          1. Solicitar Aprobación 
-        </h2>
-        <img src="/icon/info2.svg" alt="Info" class="ml-2 w-4 h-4 cursor-pointer"
-          @mouseover="mostrarModalAprobar = true"
-          @mouseleave="mostrarModalAprobar = false" />
-      </div>
-
-      <!-- Tooltip que se muestra al hacer hover sobre el ícono de información -->
-      <div v-show="mostrarModalAprobar" class="absolute left-48 mt-2 p-4 bg-white border border-gray-300 rounded-lg shadow-lg w-64 z-10">
-        <p class="text-sm text-gray-600">Se enviará tu solicitud al Programa Académico y a la Facultad.</p>
-      </div>
-
-      <div class="flex items-center justify-between mt-2">
-        <p class="text-gray-500 text-lg">Haz clic en el botón para solicitar la aprobación de tu Proyecto de Tesis</p>
-        <!-- Mostrar el estado de la solicitud -->
-        <span :class="estadoClase(solicitudEstado)" class="estado-estilo">{{ solicitudEstado }}</span>
-      </div>
-
-      <div class="mt-4">
-        <div class="flex justify-center mt-2">
-          <!-- Botón para enviar la solicitud -->
-          <button 
-            @click="primeraRevision"
-            :disabled="solicitudEstado === 'pendiente' || solicitudEstado === 'Hecho' || isLoading"  
-            :class="[
-              solicitudEstado === 'pendiente' || solicitudEstado === 'Hecho' ? 'bg-gray-300 cursor-not-allowed' : 'bg-base cursor-pointer',
-              isLoading ? 'bg-green-500' : ''
-            ]"
-            class="px-4 py-2 text-white rounded-md">
-            {{ isLoading ? 'Solicitando...' : 'Solicitar Aprobación' }}
-          </button>
-        </div>
-      </div>
-    </div>
-
-
-
+      <h3 class="text-5xl font-bold text-center text-azul">{{ textoTipiado2 }}</h3>
         <div class="mt-6 space-y-10">
+          <!-- Card 1: Solicitud-->
+          <div class="bg-white rounded-lg shadow-lg p-6 relative">
+            <div class="flex items-center justify-between">
+              <div class="flex items-center">
+                <h2 class="text-2xl font-medium text-black">1. Solicitar aprobación</h2>
+                  <img src="/icon/info2.svg" alt="Info" class="ml-2 w-4 h-4 cursor-pointer"
+                    @mouseover="mostrarModalAprobar = true"
+                    @mouseleave="mostrarModalAprobar = false" />
+              </div>
+              <span :class="['estado-estilo', `estado-${solicitudEstado.toLowerCase().replace(' ', '-')}`]" class="ml-4">{{ solicitudEstado }}</span>
+            </div>
+            <div v-show="mostrarModalAprobar" class="absolute left-40 mt-2 p-4 bg-white border border-gray-300 rounded-lg shadow-lg w-64 z-10">
+              <p class="text-sm text-gray-600">Se enviará tu solicitud al Programa Académico y a la Facultad.</p>
+            </div>
+
+            <div class="flex items-center justify-between mt-2">
+              <p class="text-gray-500 text-lg">Haz clic en el botón para enviar tu solicitud de aprobación del proyecto de tesis a la Facultad y al Programa Académico.</p>
+            </div>
+            <div class="mt-4">
+              <div class="flex justify-center mt-2">
+                <button 
+                  :disabled="isAprobacionDisabled" 
+                  :class="[ isAprobacionDisabled ? 'bg-gray-400 cursor-not-allowed' : 'bg-base', isLoading ? 'hover:bg-azul' : '']"
+                  class="px-4 py-2 text-white rounded-md"
+                  @click="solicitarAprobacion">
+                  {{ isLoading ?'Solicitando...' : 'SOLICITAR APROBACIÓN' }}
+                </button>
+              </div>
+            </div>
+          </div>
+
           <!-- Card 2: Documentos -->
           <div class="bg-white rounded-lg shadow-lg p-6">
-            <h2 class="text-2xl font-medium text-black">2. Documentos para la Aprobacion del Proyecto de Tesis</h2>
-
-            <!-- Listado de documentos -->
+            <h2 class="text-2xl font-medium text-black">2. Documentos para la aprobacion del proyecto de tesis</h2>
             <div class="mt-4 space-y-6">
-              <div v-for="(documento, index) in documentos" :key="index"
-                class="bg-gray-50 p-4 border border-gray-200 rounded-md flex items-center justify-between">
-                <!-- Nombre del documento -->
+              <div v-for="(documento, index) in documentos" :key="index" class="bg-gray-50 p-4 border border-gray-200 rounded-md flex items-center justify-between">
                 <span class="text-black flex-1">{{ documento.nombre }}</span>
-
                 <div class="flex space-x-4 items-center">
                   <!-- Botón de Ver -->
                   <a v-if="documento.estado === 'Hecho'" :href="documento.documentoUrl" target="_blank"
                     class="px-4 py-2 border rounded text-gray-600 border-gray-400 hover:bg-gray-100">
                     <i class="fas fa-eye mr-2"></i> Ver
                   </a>
-
                   <!-- Botón de Descargar -->
                   <a v-if="documento.estado === 'Hecho'" :href="documento.documentoUrl" download
                     class="px-4 py-2 border rounded text-gray-600 border-gray-400 hover:bg-gray-100">
                   <i class="fas fa-download mr-2"></i> Descargar
                   </a>
-
-                  <!-- Estado del documento -->
                   <span :class="estadoClase(documento.estado)" class="estado-estilo">{{ documento.estado }}</span>
                 </div>
               </div>
@@ -268,6 +236,7 @@ onMounted(() => {
               Siguiente
             </button>
           </div>
+
         </div>
     </div>
   </template>
@@ -277,11 +246,18 @@ onMounted(() => {
 .estado-estilo {
   padding: 0.25rem 0.5rem;
   font-size: 0.875rem;
-  font-weight: 400;
   border-radius: 0.375rem;
   display: inline-block;
 }
+.estado-pendiente {
+  background-color: #8898aa;
+  color: #ffffff;
+}
 
+.estado-no-solicitado {
+  background-color: #718096;
+  color: #ffffff;
+}
 .break-all {
   word-break: break-all;
 }
