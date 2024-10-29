088ef9495e221f710d0128182a38b14f33b6df8c
LUCIANO131231 <2018110461@udh.edu.pe>
2024-10-29 11:56:28 -0500

    tabla para ver documentos aprobados por todas las areas

--- src/views/Estudiante/ProgresoProyecto.vue
+++ src/views/Estudiante/ProgresoProyecto.vue
@@ -2,35 +2,28 @@
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
 import { useAuthStore } from '@/stores/auth';
-import IconCerrar from '@/components/icons/IconCerrar.vue';
 
 // Definición de tipo para documentos
 interface Document {
   nombre: string;
   urlView: string;
   urlDownload: string;
+  fechaEmision: string;
+  emisor: string;
 }
 
 // Propiedades reactivas
 const startDate = ref<string>('');
 const endDate = ref<string>('');
-const showDocumentModal = ref<boolean>(false);
+// const showDocumentModal = ref<boolean>(false);
 const authStore = useAuthStore();
 
 // Array de documentos con URLs de "Ver" y "Descargar"
-const documents = ref<Document[]>([
-  { nombre: "Carta de Aceptación", urlView: "", urlDownload: "" },
-  { nombre: "Conformidad Asesor", urlView: "", urlDownload: "" },
-  { nombre: "Designación de Jurados", urlView: "", urlDownload: "" },
-  { nombre: "Conformidad Jurado Presidente", urlView: "", urlDownload: "" },
-  { nombre: "Conformidad Jurado Secretario", urlView: "", urlDownload: "" },
-  { nombre: "Conformidad Jurado Vocal", urlView: "", urlDownload: "" },
-  { nombre: "Aprobación de Tesis", urlView: "", urlDownload: "" },
-]);
+const documents = ref<Document[]>([]);
 
 // Función para abrir y cerrar el modal
-const openDocumentModal = () => (showDocumentModal.value = true);
-const closeDocumentModal = () => (showDocumentModal.value = false);
+// const openDocumentModal = () => (showDocumentModal.value = true);
+// const closeDocumentModal = () => (showDocumentModal.value = false);
 
 // Función para obtener las fechas y URLs de documentos de la API
 const fetchThesisInfo = async () => {
@@ -42,26 +35,57 @@ const fetchThesisInfo = async () => {
     endDate.value = response.data.aprobacion_tesis.doc_fecha;
 
     // Actualización de URLs de documentos
-    documents.value[0].urlView = `${import.meta.env.VITE_URL_VIEW_LETTER}/${response.data.designacion_asesor.doc_id}`;
-    documents.value[0].urlDownload = `${import.meta.env.VITE_URL_DOWNLOAD_LETTER}/${response.data.designacion_asesor.doc_id}`;
-
-    documents.value[1].urlView = `${import.meta.env.VITE_URL_VIEW_CPA}/${response.data.conformidad_asesor.doc_id}`;
-    documents.value[1].urlDownload = `${import.meta.env.VITE_URL_DOWNLOAD_CPA}/${response.data.conformidad_asesor.doc_id}`;
-
-    documents.value[2].urlView = `${import.meta.env.VITE_URL_VIEW_OFFICEJURADO}/${response.data.desigancion_jurados.doc_id}`;
-    documents.value[2].urlDownload = `${import.meta.env.VITE_URL_DOWNLOAD_OFFICEJURADO}/${response.data.desigancion_jurados.doc_id}`;
-
-    documents.value[3].urlView = `${import.meta.env.VITE_URL_VIEW_CPA}/${response.data.conformidad_jurado_pres.doc_id}`;
-    documents.value[3].urlDownload = `${import.meta.env.VITE_URL_DOWNLOAD_CPA}/${response.data.conformidad_jurado_pres.doc_id}`;
-
-    documents.value[4].urlView = `${import.meta.env.VITE_URL_VIEW_CPA}/${response.data.conformidad_jurado_secre.doc_id}`;
-    documents.value[4].urlDownload = `${import.meta.env.VITE_URL_DOWNLOAD_CPA}/${response.data.conformidad_jurado_secre.doc_id}`;
-
-    documents.value[5].urlView = `${import.meta.env.VITE_URL_VIEW_CPA}/${response.data.conformidad_jurado_vocal.doc_id}`;
-    documents.value[5].urlDownload = `${import.meta.env.VITE_URL_DOWNLOAD_CPA}/${response.data.conformidad_jurado_vocal.doc_id}`;
-
-    documents.value[6].urlView = `${import.meta.env.VITE_URL_VIEW_AFACULTAD}/${response.data.aprobacion_tesis.doc_id}`;
-    documents.value[6].urlDownload = `${import.meta.env.VITE_URL_DOWNLOAD_AFACULTAD}/${response.data.aprobacion_tesis.doc_id}`;
+    documents.value = [
+      {
+        nombre: response.data.designacion_asesor.nombre_doc,
+        emisor: response.data.designacion_asesor.doc_emisor,
+        fechaEmision: response.data.designacion_asesor.doc_fecha,
+        urlView: `${import.meta.env.VITE_URL_VIEW_LETTER}/${response.data.designacion_asesor.doc_id}`,
+        urlDownload: `${import.meta.env.VITE_URL_DOWNLOAD_LETTER}/${response.data.designacion_asesor.doc_id}`,
+      },
+      {
+        nombre: response.data.conformidad_asesor.nombre_doc,
+        emisor: response.data.conformidad_asesor.doc_emisor,
+        fechaEmision: response.data.conformidad_asesor.doc_fecha,
+        urlView: `${import.meta.env.VITE_URL_VIEW_CPA}/${response.data.conformidad_asesor.doc_id}`,
+        urlDownload: `${import.meta.env.VITE_URL_DOWNLOAD_CPA}/${response.data.conformidad_asesor.doc_id}`,
+      },
+      {
+        nombre: response.data.desigancion_jurados.nombre_doc,
+        emisor: response.data.desigancion_jurados.doc_emisor,
+        fechaEmision: response.data.desigancion_jurados.doc_fecha,
+        urlView: `${import.meta.env.VITE_URL_VIEW_OFFICEJURADO}/${response.data.desigancion_jurados.doc_id}`,
+        urlDownload: `${import.meta.env.VITE_URL_DOWNLOAD_OFFICEJURADO}/${response.data.desigancion_jurados.doc_id}`,
+      },
+      {
+        nombre: response.data.conformidad_jurado_pres.nombre_doc,
+        emisor: response.data.conformidad_jurado_pres.doc_emisor,
+        fechaEmision: response.data.conformidad_jurado_pres.doc_fecha,
+        urlView: `${import.meta.env.VITE_URL_VIEW_CPA}/${response.data.conformidad_jurado_pres.doc_id}`,
+        urlDownload: `${import.meta.env.VITE_URL_DOWNLOAD_CPA}/${response.data.conformidad_jurado_pres.doc_id}`,
+      },
+      {
+        nombre: response.data.conformidad_jurado_secre.nombre_doc,
+        emisor: response.data.conformidad_jurado_secre.doc_emisor,
+        fechaEmision: response.data.conformidad_jurado_secre.doc_fecha,
+        urlView: `${import.meta.env.VITE_URL_VIEW_CPA}/${response.data.conformidad_jurado_secre.doc_id}`,
+        urlDownload: `${import.meta.env.VITE_URL_DOWNLOAD_CPA}/${response.data.conformidad_jurado_secre.doc_id}`,
+      },
+      {
+        nombre: response.data.conformidad_jurado_vocal.nombre_doc,
+        emisor: response.data.conformidad_jurado_vocal.doc_emisor,
+        fechaEmision: response.data.conformidad_jurado_vocal.doc_fecha,
+        urlView: `${import.meta.env.VITE_URL_VIEW_CPA}/${response.data.conformidad_jurado_vocal.doc_id}`,
+        urlDownload: `${import.meta.env.VITE_URL_DOWNLOAD_CPA}/${response.data.conformidad_jurado_vocal.doc_id}`,
+      },
+      {
+        nombre: response.data.aprobacion_tesis.nombre_doc,
+        emisor: response.data.aprobacion_tesis.doc_emisor,
+        fechaEmision: response.data.aprobacion_tesis.doc_fecha,
+        urlView: `${import.meta.env.VITE_URL_VIEW_AFACULTAD}/${response.data.aprobacion_tesis.doc_id}`,
+        urlDownload: `${import.meta.env.VITE_URL_DOWNLOAD_AFACULTAD}/${response.data.aprobacion_tesis.doc_id}`,
+      },
+    ];
   } catch (error) {
     console.error("Error al obtener la información de la tesis:", error);
   }
@@ -75,13 +99,11 @@ onMounted(() => {
 
 <template>
   <div class="min-h-screen bg-gray-50 p-8">
-      <div class="flex flex-col lg:flex-row items-center justify-center space-y-8 lg:space-x-16 w-full max-w-7xl mx-auto">
-        <!-- Imagen SVG a la izquierda -->
-        <div class="w-full lg:w-full flex justify-center items-center h-full">
-        <img src="/img/e1.svg" alt="Descripción de la imagen" class="w-full h-full object-contain max-h-screen" />
+    <div class="flex flex-col lg:flex-row items-center justify-center space-y-8 lg:space-x-16 w-full max-w-7xl mx-auto mb-5">
+      <!-- Imagen SVG a la izquierda -->
+      <div class="w-full lg:w-full flex justify-center items-center h-full">
+        <img src="/img/e1.svg" alt="Descripción de la imagen" class="w-96 h-full object-contain max-h-screen" />
       </div>
-
-
 
       <!-- Contenido central -->
       <div class="flex-grow flex flex-col space-y-8 items-center text-center mt-2 w-full lg:w-2/3">
@@ -103,57 +125,57 @@ onMounted(() => {
 
         <!-- Botones de acción -->
         <div class="flex justify-center gap-6">
-          <button 
-            @click="openDocumentModal" 
-            class="px-4 py-2 md:px-8 md:py-4 bg-baseClarito text-white rounded-lg hover:bg-base transition text-lg md:text-xl">
-            Ver mis documentos de proyecto de tesis
-          </button>
           <button class="px-4 py-2 md:px-8 md:py-4 bg-gray-200 text-gray-800 rounded-lg hover:bg-baseClarito transition text-lg md:text-xl">
             Ir a Informe Final
           </button>
         </div>
       </div>
     </div>
-    <!-- Modal de documentos -->
-    <div v-if="showDocumentModal" 
-        class="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto bg-gray-900 bg-opacity-50 backdrop-blur-sm transition-opacity duration-300 ease-out" 
-        @click.self="closeDocumentModal">
-      <div class="relative w-full max-w-lg mx-4 sm:mx-6 md:mx-8 lg:mx-auto p-4 bg-white rounded-lg shadow-lg max-h-full overflow-y-auto">
-        <div class="flex justify-end items-start">
-          <button class="absolute top-0 right-0 m-2 text-gray-900 hover:scale-75 transition-transform duration-150 ease-in-out" @click="closeDocumentModal">
-            <IconCerrar />
-          </button>
-        </div>
-        <div class="p-4 sm:p-6 md:p-8">
-          <h5 class="text-xl sm:text-2xl font-medium text-gray-900 text-center">Documentos Adjuntos</h5>
-          <div class="space-y-4 mt-6">
-            <div v-for="(doc, index) in documents" :key="index" class="flex flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-2 items-center justify-between">
-              <p class="text-gray-600 text-base sm:text-lg">{{ doc.nombre }}</p>
-              <div class="flex flex-row space-x-4">
-                <!-- Botón Ver -->
+    
+    <!-- Tabla de documentos aprobados por todas las areas -->
+    <div class="bg-white rounded-lg shadow-lg p-6 relative">
+      <div class="overflow-x-auto mt-4">
+        <table class="min-w-full bg-white border border-gray-200 rounded-md shadow">
+          <thead class="min-w-full leading-normal">
+            <tr class="text-center text-azul border-b-2 bg-gray-300">
+              <th class="px-4 py-2 text-left tracking-wider">DOCUMENTOS</th>
+              <th class="px-4 py-2 text-left tracking-wider">EMISOR</th>
+              <th class="px-4 py-2 text-left tracking-wider">FECHA EMISIÓN</th>
+              <th class="px-4 py-2 tracking-wider">ACCIÓN</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(doc, index) in documents" :key="index" class="border-b border-gray-200 text-left">
+              <td class="px-3 py-4 text-base text-gray-600">
+                <p class="block text-wrap w-80 uppercase">{{ doc.nombre || "Sin documento"}}</p>
+              </td>
+              <td class="px-4 py-4 text-base text-gray-600">
+                <p class="block text-wrap w-72 uppercase">{{ doc.emisor || "Sin emisor" }}</p>
+              </td>
+              <td class="px-4 py-4 text-base text-gray-600 font-medium">
+                <p class="block text-wrap w-24">{{ doc.fechaEmision || "Sin fecha de emisión" }}</p>
+              </td>
+              <td class="px-4 py-2 justify-center flex flex-col space-y-2 w-full md:flex-row md:space-y-0 md:space-x-2">
                 <a 
                   :href="doc.urlView" 
                   target="_blank" 
-                  class="flex items-center px-3 py-2 sm:px-4 border rounded text-gray-600 border-gray-400 hover:bg-gray-100">
+                  class="w-full items-center mr-2 px-4 py-2 border rounded text-gray-600 border-gray-400 hover:bg-gray-200 md:w-auto justify-center">
                   <i class="fas fa-eye mr-2"></i> Ver
                 </a>
-                <!-- Botón Descargar -->
                 <a 
                   :href="doc.urlDownload" 
                   download 
-                  class="flex items-center px-3 py-2 sm:px-4 border rounded text-gray-600 border-gray-400 hover:bg-gray-100">
+                  class="w-full items-center text-center px-4 py-2 border rounded text-gray-600 border-gray-400 hover:bg-gray-200 md:w-auto justify-center">
                   <i class="fas fa-download mr-2"></i> Descargar
                 </a>
-              </div>
-            </div>
-          </div>
-          <div class="flex justify-center p-6 mt-2 border-t border-gray-200">
-            <button class="px-4 py-2 text-lg font-thin text-white bg-[#5d6d7e] rounded-2xl" @click="closeDocumentModal">Cancelar</button>
-          </div>
-        </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
-
+    
+   
   </div>
 </template>
 
