969f77e8c59c7f524feb38f64eeb3f30f75d4f45
MarvinH10 <marvinhectorcamposdeza@gmail.com>
2024-12-08 02:20:05 -0500

    Componente Estados

--- src/components/Estados.vue
+++ src/components/Estados.vue
@@ -6,17 +6,34 @@ export default {
       required: true,
     },
   },
+  computed: {
+    estadoClase() {
+      const estadoLowerCase = this.estado.toLowerCase();
+      switch (estadoLowerCase) {
+        case "pendiente":
+          return "bg-gray-400 text-white";
+        case "aceptado":
+          return "bg-green-500 text-white";
+        case "tramitado":
+          return "bg-green-500 text-white";
+        case "rechazado":
+          return "bg-red-500 text-white";
+        default:
+          return "bg-gray-200 text-gray-700";
+      }
+    },
+  },
   methods: {
     capitalizeFirstLetter(text) {
-      if (!text) return "";
-      return text.charAt(0).toUpperCase() + text.slice(1);
+      if (!text) return "Desconocido";
+      return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
     },
   },
 };
 </script>
 
 <template>
-  <span class="bg-green-500 text-white estado-estilo">
+  <span :class="['estado-estilo', estadoClase]">
     {{ capitalizeFirstLetter(estado) }}
   </span>
 </template>
@@ -29,8 +46,28 @@ export default {
   border-radius: 0.375rem;
 }
 
-.estado-pendiente {
-  background-color: #8898aa;
-  color: #ffffff;
+.bg-gray-400 {
+  background-color: #9ca3af;
+  color: white;
+}
+
+.bg-red-500 {
+  background-color: #e53e3e;
+  color: white;
+}
+
+.bg-green-500 {
+  background-color: #38a169;
+  color: white;
+}
+
+.bg-orange-500 {
+  background-color: #ed8936;
+  color: white;
+}
+
+.bg-gray-200 {
+  background-color: #e2e8f0;
+  color: #4a5568;
 }
 </style>
