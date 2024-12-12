0f1e233e896b6b2e4903884ce55b5387457ff08c
MarvinH10 <marvinhectorcamposdeza@gmail.com>
2024-12-12 00:26:44 -0500

    sobreescribiendo dataInfo

--- src/components/DocumentTabla.vue
+++ src/components/DocumentTabla.vue
@@ -10,8 +10,9 @@
     </thead>
     <tbody v-if="documents && documents.length > 0">
       <tr v-for="(doc, index) in documents" :key="index" class="border-b border-gray-200 text-center uppercase">
-        <td class="px-4 py-2 text-md text-left text-gray-600">
-          <p class="">{{ doc.nombre || "Sin documento" }}</p>
+        <td class="px-4 py-2 text-md text-left text-gray-600 flex items-center space-x-2">
+          <!-- <p class="">{{ doc.nombre || "Sin documento" }}</p> -->
+          <p class="">{{ documentCambioDeTexto(doc.nombre) }}</p>
         </td>
         <td class="px-4 py-2 text-md text-left text-gray-600">
           <p class="">{{ doc.emisor || "Sin emisor" }}</p>
@@ -20,13 +21,11 @@
           <p class="truncate">{{ doc.fechaEmision || "Sin fecha" }}</p>
         </td>
         <td class="px-4 py-4 justify-center flex flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-2 w-full">
-          <a 
-            :href="doc.urlView" target="_blank"
+          <a :href="doc.urlView" target="_blank"
             class="flex items-center px-2 py-1 text-sm border rounded text-gray-600 border-gray-400 hover:bg-gray-100 w-full md:w-auto justify-center">
             <i class="fas fa-eye mr-2 text-xs"></i> Ver
           </a>
-          <a 
-            :href="doc.urlDownload" download
+          <a :href="doc.urlDownload" download
             class="flex items-center px-2 py-1 text-sm border rounded text-gray-600 border-gray-400 hover:bg-gray-100 w-full md:w-auto justify-center">
             <i class="fas fa-download mr-2 text-xs"></i> Descargar
           </a>
@@ -51,6 +50,21 @@ interface Document {
   urlDownload: string;
   fechaEmision: string;
   emisor: string;
+}
+
+function documentCambioDeTexto(documentName: string): string {
+  const documentoTextoMapeo: Record<string, string> = {
+    'Oficio de conformidad - Asesor': 'OFICIO MULTIPLE DE DESIGNACION DE JURADOS PARA LA REV. DEL TRABAJO DE INV. (TESIS) - POR EL PROGRAMA ACADEMICO',
+    'Oficio múltiple de designación de jurados': 'ACTA DE CONFORMIDAD DEL PROYECTO DE INVESTIGACION - POR EL JURADO PRESIDENTE',
+    'Oficio de conformidad - Presidente': 'ACTA DE CONFORMIDAD DEL PROYECTO DE INVESTIGACION - POR EL JURADO SECRETARIO',
+    'Oficio de conformidad - Secretario': 'ACTA DE CONFORMIDAD DEL PROYECTO DE INVESTIGACION - POR EL JURADO VOCAL',
+    'Oficio de conformidad - Vocal': 'OFICIO DE APROBACIÓN DEL TRABAJO DE INVESTIGACIÓN (TESIS)',
+    'Resolución de aprobación de trabajo de investigación (Tesis)': 'RESOLUCION DE APROBACIÓN DEL TRABAJO DE INVESTIGACIÓN (TESIS)',
+    'Carta de aceptación': 'primero',
+    'Resolución de designación de asesor': 'segundo',
+  };
+
+  return documentoTextoMapeo[documentName] || documentName || 'Sin documento';
 }
 
 defineProps<{ documents: Document[] }>();
