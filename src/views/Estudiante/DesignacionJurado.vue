fbcdbfa1e1cd0c8fc091ceedfbc001f1f91a8b34
LUCIANO131231 <2018110461@udh.edu.pe>
2024-12-11 19:25:12 -0500

    cambiando nombre del documento y crear componente skeleton para designar jurados

--- src/views/Estudiante/DesignacionJurado.vue
+++ src/views/Estudiante/DesignacionJurado.vue
@@ -11,6 +11,7 @@ import ButtonRequest from "@/components/ButtonRequest.vue";
 import JuradoTabla from "@/components/JuradoTabla.vue";
 import { useTypewriter } from "@/composables/useTypewriter";
 import NavigationButton from "@/components/NavigationButton.vue";
+import SkeletonDesignarJurados from "@/components/SkeletonDesignarJurados.vue";
 
 // extrayendo funcionn del composable
 const { textoTipiado, typeWriter } = useTypewriter(
@@ -130,68 +131,7 @@ onMounted(() => {
 });
 </script>
 <template>
-  <template v-if="load">
-    <div class="flex-1 p-10 bg-gray-100 min-h-full">
-      <div
-        class="flex justify-center items-center content-center px-14 flex-col"
-      >
-        <h3
-          class="bg-gray-200 h-10 w-full rounded-md duration-200 skeleton-loader"
-        ></h3>
-        <br />
-      </div>
-      <div class="mt-6 space-y-10">
-        <div
-          class="bg-white rounded-md shadow-lg p-6 h-auto mt-4 animate-pulse duration-200"
-        >
-          <div class="block space-y-4">
-            <h2
-              class="bg-gray-200 h-6 w-2/4 rounded-md skeleton-loader duration-200 mb-10"
-            ></h2>
-            <h2
-              class="bg-gray-200 h-10 w-64 mx-auto rounded-md skeleton-loader duration-200"
-            ></h2>
-          </div>
-        </div>
-        <div
-          class="bg-white rounded-md shadow-lg p-6 h-auto mt-4 animate-pulse duration-200"
-        >
-          <div class="block space-y-4">
-            <h2
-              class="bg-gray-200 h-6 w-2/4 rounded-md skeleton-loader duration-200 mb-10"
-            ></h2>
-            <h2
-              class="bg-gray-200 h-28 w-2/4 mx-auto rounded-md skeleton-loader duration-200"
-            ></h2>
-          </div>
-        </div>
-        <div
-          class="bg-white rounded-md shadow-lg p-6 h-auto mt-4 animate-pulse duration-200"
-        >
-          <div class="block space-y-5">
-            <h2
-              class="bg-gray-200 h-6 w-2/4 rounded-md skeleton-loader duration-200"
-            ></h2>
-            <h2
-              class="bg-gray-200 h-20 w-full rounded-md skeleton-loader duration-200"
-            ></h2>
-          </div>
-        </div>
-        <div class="flex justify-between">
-          <div class="block space-y-5">
-            <h2
-              class="px-4 py-2 h-11 w-28 rounded-md skeleton-loader duration-200"
-            ></h2>
-          </div>
-          <div class="block space-y-5">
-            <h2
-              class="px-4 py-2 h-11 w-28 rounded-md skeleton-loader duration-200"
-            ></h2>
-          </div>
-        </div>
-      </div>
-    </div>
-  </template>
+  <template v-if="load"><SkeletonDesignarJurados/></template>
   <template v-else>
     <div class="flex-1 p-10 font-Roboto bg-gray-100 min-h-full">
       <h3 class="text-4xl font-bold text-center text-azul">
@@ -285,7 +225,7 @@ onMounted(() => {
           <!-- oficion multiple emitido por el programa academico -->
           <div class="mt-4 space-y-4">
             <DocumentCard
-              titulo="Oficio Múltiple de Designación de Jurados para la Rev. del Trabajo de Inv. (Tesis) - por el Programa Académico"
+              titulo="OFICIO MULTIPLE DE DESIGNACION DE JURADOS PARA LA REV. DEL TRABAJO DE INV. (TESIS) - POR EL PROGRAMA ACADEMICO"
               :estado="obtener?.estado || ''"
               :id="obtener?.docof_id ?? ''"
               :view="VIEW_OFFICEJURADO"
