e216d6d7dca07d5ac9bcdbdd9d5878f5e431b667
Mary22Martel <2020210311@gmail.com>
2024-12-13 00:46:47 -0500

    iconos en docs del 3 de vri

--- src/views/Estudiante/ConformidadVRI.vue
+++ src/views/Estudiante/ConformidadVRI.vue
@@ -278,7 +278,10 @@ onMounted(() => {
           <div class="mt-4 space-y-4">
             <div class="bg-gray-50 p-4 border border-gray-200 rounded-md">
               <div class="flex flex-col md:flex-row justify-between md:items-center text-gray-700 font-medium">
+                <div class="flex items-center space-x-4 text-gray-700 font-medium">
+                <i class="fas fa-file-alt text-[#39B49E] text-2xl"></i>
                 <span class="flex-1 text-xm bg-gray-50">VERIFICACIÓN DE DOCUMENTOS</span>
+                </div>
                 <div class="flex flex-col md:flex-row items-start md:items-center justify-end w-full md:w-auto space-y-2 md:space-y-0 md:space-x-4">
                   <div v-if="primerFiltro.fil_estado === 'aprobado'" class="flex flex-col space-y-2 w-full md:flex-row md:space-y-0 md:space-x-2"></div>
                   <span v-else class="text-gray-500 italic text-sm">Solicitud en espera</span>
